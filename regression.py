4db7d12d8d1e7110949b578d9087ebcb79b922fd
Bridget McInnes <btmcinnes@vcu.edu>
2024-03-08 16:16:28 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,7 +1,30 @@
+"""
+File: regression.py
+
+Author: Bridget McInnes
+
+Date: October 2023
+
+Description: This script performs a regression over the colours data. 
+
+Usage: python regression.py <colours csv file> <fig output file> <Regression model>
+
+Options for Regression modle:
+  1. LinearRegression
+  2. RandomForestRegression
+  3. SVR
+  4. DecisionTree Regressor (default)
+
+Example:
+  python3 regression.py colours_mlr_expanded.csv figure.png LinearRegression
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
+
+from sys import argv
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -15,7 +38,8 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
 # load dataset
-dataset = np.loadtxt('colours_mlr_expanded.csv', delimiter=',')
+dataset = np.loadtxt(argv[1], delimiter=',')
+#dataset = np.loadtxt('colours_mlr_expanded.csv', delimiter=',')
 #dataset = np.loadtxt('colours_mlr_rgb.csv', delimiter=',')
 
 # Randomly shuffle the elements in the dataset
@@ -29,17 +53,23 @@ X = dataset[:, 0:3]  # All columns except the first one are features
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 # Print the shapes of the splits to verify
-print("X_train shape:", X_train.shape)
-print("y_train shape:", y_train.shape)
-print("X_test shape:", X_test.shape)
-print("y_test shape:", y_test.shape)
+#print("X_train shape:", X_train.shape)
+#print("y_train shape:", y_train.shape)
+#print("X_test shape:", X_test.shape)
+#print("y_test shape:", y_test.shape)
 
 
 ## Create a Linear Regression model
-#model = LinearRegression() 
-#model = SVR(kernel='poly') 
-#model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=0) 
 model = DecisionTreeRegressor()
+if len(argv) < 3:
+    if(argv[3] == "LinearRegression"):
+        model = LinearRegression() 
+    if(argv[3] == "SVR"):
+        model = SVR(kernel='poly') 
+    if(argv[3] == "RandomForestRegression"):
+        model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=0)
+    else:
+        model = DecisionTreeRegressor()
 
 # Train the model
 model.fit(X_train, y_train)
@@ -91,5 +121,5 @@ feature_names = ["COC", "CC", "CP"]
 plt.figure(figsize=(20, 15))
 plot_tree(trees[best_index], feature_names=feature_names, filled=True)
 
-plt.savefig('decision_tree.png', dpi=300)
+plt.savefig(argv[2], dpi=300)
 
